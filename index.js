f36fca5c471b727ee59492147c0711274cd60e91
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-23 12:36:43 -0600

    psuedocoded next workflow; Created three variables for a persons firstName,lastName, and humanAge.

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ const secondMessage = ' ';
 const thirdMessage = `World!`
 let fullMessage = `${firstMessage}${secondMessage}${thirdMessage}`;
 console.log(fullMessage);
+
+//Create 3 Variables for somenes name and age
+//(1)John, (2)Smith, (3)54.
+const firstName = "John";
+const lastName = "Smith"
+const humanAge = 54;
+
+//Use JavaScript to change the last name to `Doe`.
+//Display in an alert box `My name is [someone's full name] and I am [age] years old.`

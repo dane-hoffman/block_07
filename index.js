e844d326c19a8e1d27eb4e1b3d0a6c765329af49
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-23 12:32:29 -0600

    Concantenate three strings 'hello' ' ' 'world' and display as one string through using separate variables.

---
+++ index.js
@@ -0,0 +1,13 @@
+const numberOfCows = 50;
+
+const numberOfUmbrellas = 10 * 3;
+console.log(numberOfUmbrellas);
+
+const billboardTitle = `Jimmy's Greates Hits`;
+console.log(billboardTitle) 
+
+const firstMessage = `Hello`;
+const secondMessage = ' ';
+const thirdMessage = `World!`
+let fullMessage = `${firstMessage}${secondMessage}${thirdMessage}`;
+console.log(fullMessage);

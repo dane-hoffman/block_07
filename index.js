66aaeb49bae33bffcdc8ddb3073a8731dbc31ddb
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-10-23 12:40:48 -0600

    use javascript to change last name to 'Doe'.

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ console.log(fullMessage);
 //Create 3 Variables for somenes name and age
 //(1)John, (2)Smith, (3)54.
 const firstName = "John";
-const lastName = "Smith"
+let lastName = "Smith"
 const humanAge = 54;
 
 //Use JavaScript to change the last name to `Doe`.
+lastName = "Doe";
+
 //Display in an alert box `My name is [someone's full name] and I am [age] years old.`
+alert(`My name is ${firstName} ${lastName} and I am ${humanAge} years old.`);
